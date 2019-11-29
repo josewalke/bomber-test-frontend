ba006f45a084fad79f72c541f3e738636bb4bccb
Jose Perez <josejperez@singularfactory.net>
2019-11-29 17:00:41 +0000

    insertar url  en axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://reboot-nuxt-template.herokuapp.com/api/'
+  baseURL: 'https://reboot-jose-final.herokuapp.com/api/'
 })
