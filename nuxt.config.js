788216289d4e484a8ef3be55114c9f7ff4b5160f
Fico Aguiar <aguiar.f@gmail.com>
2019-12-11 19:25:04 +0000

    Revert "adding page title"
    This reverts commit 341394fc4e41d86cf1ce5a4ffe79e876fe107349.

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s | BomberTest',
-    title: 'BomberTest',
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
