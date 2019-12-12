97fc6d650d1a15d9571438c8ba1196cda5135af1
Jose Perez <josejperez@singularfactory.net>
2019-12-12 18:23:12 +0000

    Profile user with Photo

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,5 +1,30 @@
 <template>
-  <v-app>{{ usuario }}</v-app>
+  <v-row>
+    <v-col cols="10" offset="1">
+      <v-card class="mx-auto">
+        <v-img
+          src="https://icon-library.net/images/avatar-icon-png/avatar-icon-png-8.jpg"
+          height="200px"
+        ></v-img>
+
+        <v-card-title>
+          {{ usuario.name }}
+          {{ usuario.lastName }}
+        </v-card-title>
+
+        <v-card-subtitle>
+          <div>
+            <font-awesome-icon icon="envelope" />
+            {{ usuario.email }}
+          </div>
+          <div>
+            <font-awesome-icon icon="phone-square" />
+            {{ usuario.phone }}
+          </div>
+        </v-card-subtitle>
+      </v-card></v-col
+    >
+  </v-row>
 </template>
 
 <script>
