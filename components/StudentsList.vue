1b758a1b66ac192d5db909950072b83bd94c49a1
Fico Aguiar <aguiar.f@gmail.com>
2020-02-11 20:32:28 +0000

    studen list filters

--- components/StudentsList.vue
+++ components/StudentsList.vue
@@ -2,15 +2,56 @@
   <div>
     <h1>Alumnos</h1>
     <v-row>
-      <v-col cols="4">
-        <v-text-field v-model="f_nombre" label="Nombre"></v-text-field>
+      <v-col cols="2">
+        <v-switch
+          v-model="switch1"
+          :label="`activos: ${switch1.toString()}`"
+          @change="solo_activos"
+        ></v-switch>
       </v-col>
-      <v-col cols="4">
-        <v-text-field v-model="f_apellido" label="Apellido"></v-text-field>
+      <v-col cols="2">
+        <v-switch
+          v-model="switch2"
+          :label="`inactivos: ${switch2.toString()}`"
+          @change="solo_inactivos"
+        ></v-switch>
       </v-col>
-      <v-col cols="4">
-        <br />
-        <v-btn @click="filtrar">Filtrar</v-btn>
+      <v-col cols="2">
+        <v-text-field
+          v-model="f_nombre"
+          label="Nombre"
+          @keyup="filtrar"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-text-field
+          v-model="f_apellido"
+          label="Apellido"
+          @keyup="filtrar"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col vols="2">
+        <v-switch
+          v-model="plan[0]"
+          :label="`Basic`"
+          @change="select_basic"
+        ></v-switch>
+      </v-col>
+      <v-col vols="2">
+        <v-switch
+          v-model="plan[1]"
+          :label="`Pro`"
+          @change="select_pro"
+        ></v-switch>
+      </v-col>
+      <v-col vols="2">
+        <v-switch
+          v-model="plan[2]"
+          :label="`Premium`"
+          @change="select_premium"
+        ></v-switch>
       </v-col>
     </v-row>
     <v-simple-table>
@@ -25,8 +66,8 @@
             <th class="text-left">Activo</th>
           </tr>
         </thead>
-        <tbody v-if="filtro">
-          <tr v-for="(item, idx) in student" :key="idx">
+        <tbody>
+          <tr v-for="(item, idx) in filter" :key="idx">
             <td class="text-truncate" style="max-width: 150px;">
               {{ item.name }}
             </td>
@@ -42,81 +83,6 @@
               ></v-select>
             </td>
             <td>
-              <v-checkbox
-                v-model="item.active"
-                color="primary"
-                @change="change_active(idx)"
-              ></v-checkbox>
-            </td>
-          </tr>
-        </tbody>
-        <tbody v-else>
-          <tr v-for="(item, idx) in student" :key="idx">
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-              class="text-truncate"
-              style="max-width: 150px;"
-            >
-              {{ item.name }}
-            </td>
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-            >
-              {{ item.lastName }}
-            </td>
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-            >
-              {{ item.email }}
-            </td>
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-            >
-              {{ item.phone }}
-            </td>
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-            >
-              <v-select
-                :items="items"
-                :label="item.suscription_type"
-                @input="setSelected"
-                @change="change_suscription(idx)"
-              ></v-select>
-            </td>
-            <td
-              v-if="
-                item.name.toUpperCase() === f_nombre.toUpperCase() ||
-                  item.lastName.toUpperCase() === f_apellido.toUpperCase() ||
-                  (item.name.toUpperCase() === f_nombre.toUpperCase() &&
-                    item.lastName.toUpperCase() === f_apellido.toUpperCase())
-              "
-            >
               <v-checkbox
                 v-model="item.active"
                 color="primary"
@@ -146,25 +112,163 @@ export default {
     return {
       f_nombre: '',
       f_apellido: '',
-      filtro: true,
+      filtro: false,
       items: ['Basic', 'Pro', 'Premium'],
-      seleccion: ''
+      seleccion: '',
+      filter: this.student,
+      switch1: false,
+      switch2: false,
+      plan: [false, false, false]
     }
   },
   methods: {
-    filtrar() {
-      this.filtro = false
+    filtrar(event) {
+      this.filtro = true
+      let filteredUsers = []
+      let name = this.f_nombre.toLowerCase()
+      let sirname = this.f_apellido.toLowerCase()
+      if (event.key === 'Backspace') {
+        this.filter = this.student
+      }
+      if (name.length === 0 && sirname.length === 0) {
+        this.filter.forEach(user => {
+          if (
+            user.name.toLowerCase().includes('') &&
+            user.lastName.toLowerCase().includes('')
+          ) {
+            filteredUsers.push(user)
+          }
+        })
+        this.filter = this.student
+      }
+      if (name.length > 0 && sirname.length > 0) {
+        this.filter.forEach(user => {
+          if (
+            user.name.toLowerCase().includes(name) &&
+            user.lastName.toLowerCase().includes(sirname)
+          ) {
+            filteredUsers.push(user)
+          }
+        })
+        this.filter = filteredUsers
+      }
+      if (name.length > 0 && sirname.length === 0) {
+        this.filter.forEach(user => {
+          if (user.name.toLowerCase().includes(name)) {
+            filteredUsers.push(user)
+          }
+        })
+        this.filter = filteredUsers
+      }
+      if (name.length === 0 && sirname.length > 0) {
+        this.filter.forEach(user => {
+          if (user.lastName.toLowerCase().includes(sirname)) {
+            filteredUsers.push(user)
+          }
+        })
+        this.filter = filteredUsers
+        console.log(this.fiter === filteredUsers)
+      }
+    },
+    solo_activos() {
+      console.log('switch')
+      let filter = []
+      if (this.switch2 === true) {
+        this.switch2 = false
+        this.filter = this.student
+      }
+      if (this.switch1 === true) {
+        this.filter.forEach(user => {
+          if (user.active === true) {
+            filter.push(user)
+          }
+        })
+        this.filter = filter
+      } else {
+        this.filter = this.student
+      }
+    },
+    solo_inactivos() {
+      console.log('switch')
+      let filter = []
+      if (this.switch1 === true) {
+        this.switch1 = false
+        this.filter = this.student
+      }
+      if (this.switch2 === true) {
+        this.filter.forEach(user => {
+          if (user.active !== true) {
+            filter.push(user)
+          }
+        })
+        this.filter = filter
+      } else {
+        this.filter = this.student
+      }
+    },
+
+    select_basic() {
+      if (this.plan[0] === true) {
+        let filter = []
+        this.filter = this.student
+        this.plan[1] = false
+        this.plan[2] = false
+        this.filter.forEach(user => {
+          if (user.suscription_type === 'Basic') {
+            filter.push(user)
+          }
+          this.filter = filter
+        })
+      } else {
+        this.filter = this.student
+      }
+    },
+    select_pro() {
+      if (this.plan[1] === true) {
+        let filter = []
+        this.filter = this.student
+        this.plan[0] = false
+        this.plan[2] = false
+        this.filter.forEach(user => {
+          if (user.suscription_type === 'Pro') {
+            filter.push(user)
+          }
+          this.filter = filter
+        })
+      } else {
+        this.filter = this.student
+      }
+    },
+    select_premium() {
+      if (this.plan[2] === true) {
+        let filter = []
+        this.filter = this.student
+        this.plan[0] = false
+        this.plan[1] = false
+        this.filter.forEach(user => {
+          if (user.suscription_type === 'Premium') {
+            filter.push(user)
+          }
+          this.filter = filter
+        })
+      } else {
+        this.filter = this.student
+      }
     },
     change_active(idx) {
+      console.log(this.filter[idx]._id)
+
       const body = {
-        id: this.student[idx]._id,
-        active: this.student[idx].active
+        id: this.filter[idx]._id,
+        active: this.filter[idx].active
       }
+      console.log('aqui')
+      console.log(body)
       API.change_active(body)
     },
     change_suscription(idx) {
       const body = {
-        id: this.student[idx]._id,
+        id: this.filter[idx]._id,
         suscription_type: this.seleccion
       }
       API.change_suscription(body)
