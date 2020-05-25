07c1814acb2317db715af461b7521f10c1844442
Jose Perez <josejperez@singularfactory.net>
2020-05-25 18:35:53 +0100

    correccion

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -31,7 +31,7 @@
     <v-col md="10" offset="1">
       <v-alert type="error">
         Para el correcto funcionamiento de la pagina evite usar el Google
-        Translate o selecciones la opcion "ingles"
+        Translate o seleccione la opcion "ingles"
       </v-alert>
     </v-col>
     <v-row v-if="formato != 'movil'">
