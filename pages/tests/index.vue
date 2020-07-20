dce2f0a051dd392e2896b8a3d628d15b7a38c7bf
Jose Perez <josejperez@singularfactory.net>
2020-07-20 16:29:51 +0100

    a ver si funciona

--- pages/tests/index.vue
+++ pages/tests/index.vue
@@ -129,7 +129,7 @@ export default {
   methods: {
     async testGeneration() {
       if (this.active === true) {
-        await this.$store.dispatch('createTest')
+        this.$store.dispatch('createTest')
         this.$router.push(`/tests/${this.$store.state.currentTest._id}/`)
       } else {
         alert('para realizar un test primero debe activar la cuenta')
