f122bd67bde875365e7cfc3f79641ae39fea8e64
Jose Perez <josejperez@singularfactory.net>
2020-08-20 02:15:13 +0100

    original

--- services/api.js
+++ services/api.js
@@ -20,8 +20,6 @@ export default {
     return axios.get('questions').then(response => response.data)
   },
   getAllTestById(userId) {
-    console.log('probando')
-    console.log(userId)
     return axios.get('tests/user/me/' + userId).then(response => response.data)
   },
   generateTest(token) {
