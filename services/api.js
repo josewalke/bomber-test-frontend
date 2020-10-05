922c0154bbafe41c93f5324813dd853e70b675e0
Jose Perez <josejperez@singularfactory.net>
2020-10-05 11:49:12 +0100

    pequeño retoque

--- services/api.js
+++ services/api.js
@@ -367,7 +367,7 @@ export default {
   },
   sendEmail(data) {
     console.log('hola mundo')
-    return axios.post('correo/find', data).then(response => response.data)
+    return axios.post('correo/find/', data).then(response => response.data)
   },
   new_pass(data) {
     let token = data.token
