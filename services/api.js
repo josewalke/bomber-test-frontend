54b1d9ee2d5c10d37f1f69658ea8a3c272e154fe
Jose Perez <josejperez@singularfactory.net>
2020-10-05 11:27:21 +0100

    console.log(api.js)

--- services/api.js
+++ services/api.js
@@ -366,6 +366,7 @@ export default {
     })
   },
   sendEmail(data) {
+    console.log('hola mundo')
     return axios.post('correo/find', data).then(response => response.data)
   },
   new_pass(data) {
