510d505a65e6fe1080b6c51b363c55860d8eeac5
Jose Perez <josejperez@singularfactory.net>
2020-10-06 19:15:53 +0100

    probando

--- services/api.js
+++ services/api.js
@@ -369,8 +369,8 @@ export default {
     })
   },
   sendEmail(data) {
-    // return axios.post('correo/find/', data).then(response => response.data)
-    return axios.post('users/lolo', data).then(response => response.data)
+    return axios.post('correo/find', data).then(response => response.data)
+    // return axios.post('users/lolo', data).then(response => response.data)
     // return axios.get('users').then(response => response.data)
   },
   new_pass(data) {
