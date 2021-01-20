2784c7c933590b2fdeab9774fdd147d53caede6a
Jose Perez <josejperez@singularfactory.net>
2021-01-20 21:28:00 +0100

    console.log

--- pages/tests/index.vue
+++ pages/tests/index.vue
@@ -103,7 +103,7 @@ import { mapGetters } from 'vuex'
 
 export default {
   async asyncData({ store }) {
-    // console.log(store.getters.userId)
+    console.log(store.getters.userId)
     const tests = await API.getAllTestById(store.getters.userId)
     return { tests }
   },
